e544be06fe1a12e3ad7814046023a3aaa9b3516a
Jared Bell <jared.m.bell@gmail.com>
2021-06-15 13:45:33 -0400

    Add files via upload

---
+++ pygitm/read_binary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#-------------------
+# JMB:  12/27/2019:  Short code to test reading in GITM binary file
+
+import numpy as np
+import datetime as dt
+
+print('Yep, This code ran.')
+
+
